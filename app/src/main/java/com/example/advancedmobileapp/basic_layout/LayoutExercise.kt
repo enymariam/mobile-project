4a06023ae384a2c9c45c8b88af9c2ce82433aea9
Enymariam <jammondt@gmail.com>
2025-05-26 23:30:24 +0300

    Added refresh icon

--- app/src/main/java/com/example/advancedmobileapp/basic_layout/LayoutExercise.kt
+++ app/src/main/java/com/example/advancedmobileapp/basic_layout/LayoutExercise.kt
@@ -1,5 +1,6 @@
 package com.example.advancedmobileapp.basic_layout
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,8 +23,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.advancedmobileapp.R
 import com.example.advancedmobileapp.ui.theme.AdvancedMobileAppTheme
 
 
@@ -48,14 +56,25 @@ fun LayoutExercise(modifier: Modifier = Modifier) {
 
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center) {
-                Box(modifier = Modifier.size(60.dp).weight(1f),
+                Box(modifier = Modifier.size(30.dp).weight(1f),
                     contentAlignment = Alignment.Center
                     ) {
                     Text("EUR")
                 }
-                Box(modifier = Modifier.size(60.dp).weight(1f),
+                Box(modifier = Modifier.size(30.dp).weight(1f),
                     contentAlignment = Alignment.Center) {
                     Text("USD")
+                }
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center) {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Refresh"
+                    )
                 }
             }
         }
