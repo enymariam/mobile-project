77854939a8b2b29f643e3196cff0fc5ba453106e
Enymariam <jammondt@gmail.com>
2025-05-28 21:14:49 +0300

    Cosmetics

--- app/src/main/java/com/example/advancedmobileapp/models/RatingDto.kt
+++ app/src/main/java/com/example/advancedmobileapp/models/RatingDto.kt
@@ -1,6 +1,14 @@
 package com.example.advancedmobileapp.models
 
-data class RatingDto(val id: Int, val name: String, val cuisine: String, val price_range: String, val address: String, val open_status: String, val rating: Float?, val review_count: Int)
+data class RatingDto(
+    val id: Int,
+    val name: String,
+    val cuisine: String,
+    val price_range: String,
+    val address: String,
+    val open_status: String,
+    val rating: Float?,
+    val review_count: Int)
 
 data class RatingsState(
     val loading: Boolean = false,
