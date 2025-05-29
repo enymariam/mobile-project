0b889d0682bcf28475f1850f80a70874e84ef2f7
Enymariam <jammondt@gmail.com>
2025-05-29 18:03:15 +0300

    Test if get restaurant works with hard coded id

--- app/src/main/java/com/example/advancedmobileapp/DataApi.kt
+++ app/src/main/java/com/example/advancedmobileapp/DataApi.kt
@@ -3,6 +3,7 @@ package com.example.advancedmobileapp
 import com.example.advancedmobileapp.models.RestaurantDto
 import com.example.advancedmobileapp.models.RestaurantWithAvgRatingDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface DataApi {
@@ -11,6 +12,7 @@ interface DataApi {
     suspend fun getRestaurantsWithAvgRatings(): List<RestaurantWithAvgRatingDto>
 
     // Ratings on individual restaurant
-    @GET("api/restaurants/{id}ratings")
+    @GET("api/restaurants/1/ratings")
     suspend fun getRestaurant(): List<RestaurantDto>
+    //suspend fun getRestaurant(@Path("id") id: Int): RestaurantDto?
 }
